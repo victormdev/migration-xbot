1477c658fbfdf1a5f21175976a9e951bb98a991b
Victor Macedo <victoralexandrino99@gmail.com>
2019-09-25 16:28:44 -0300

    Modelagem das rotas e adição das views (header e sidebar).

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,8 +5,8 @@ import { ProjectsComponent } from './layout/projects/projects.component';
 
 
 const routes: Routes = [
-  { path: 'projects', loadChildren: () => import('./layout/projects/projects.component').then(m => m.ProjectsComponent) }
-];
+  { path: 'projects', component: ProjectsComponent }
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/layout/layout-routing.module.ts
+++ src/app/layout/layout-routing.module.ts
@@ -4,17 +4,8 @@ import { LayoutComponent } from './layout.component';
 import { ProjectsComponent } from './projects/projects.component';
 
 const routes: Routes = [
-    {
-        path: '',
-        component: LayoutComponent,
-        children: [
-            { path: '', redirectTo: 'dashboard', pathMatch: 'prefix' },
-            { path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.component').then(m => m.DashboardComponent) },
-            { path: 'projects', loadChildren: () => import('./projects/projects.component').then(m => m.ProjectsComponent) },
-            { path: 'funcs', loadChildren: () => import('./funcs/funcs.component').then(m => m.FuncsComponent) }
-        ]
-    }
-];
+    { path: 'projects', component: ProjectsComponent }
+    ];
 @NgModule({
     imports: [RouterModule.forChild(routes)],
     exports: [RouterModule]
