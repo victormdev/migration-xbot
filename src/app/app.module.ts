03a0edf88082a6895ed93926122af7bb20514859
Victor Macedo <victoralexandrino99@gmail.com>
2019-10-03 11:38:39 -0300

    Alteração da cor da navbar e adição dos componentes e módulos no "app.module"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AdministrationComponent } from './administration/administration.component';
+import { AdministrationModule } from './administration/administration.module';
 import { FuncsComponent } from './funcs/funcs.component';
 import { ProjectsComponent } from './projects/projects.component';
+import { ProjectsModule } from './projects/projects.module';
+import { FuncsModule } from './funcs/funcs.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { DashboardModule } from './dashboard/dashboard.module';
 import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    HeaderComponent
+    HeaderComponent,
+    ProjectsComponent,
+    FuncsComponent,
+    AdministrationComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ProjectsComponent,
-    FuncsComponent,
-    AdministrationComponent,
-    DashboardComponent
-
+    ProjectsModule,
+    FuncsModule,
+    AdministrationModule,
+    DashboardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
